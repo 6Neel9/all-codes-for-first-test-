e3cc67c175c00176cdee7fbebc9c25ad89e76b27
6Neel9 <88641136+6Neel9@users.noreply.github.com>
2021-12-26 19:28:23 +0530

    Add files via upload

---
+++ arithmatic nth term/arithmatic_pro.js
@@ -0,0 +1,19 @@
+var a = 2, b = 5;
+var N = 7;
+
+var d = b - a;
+ap = a + (N-1)*d
+console.log(ap )
+
+
+
+
+
+/*var findNthTerm = (first, second, num) => {
+   var diff = second - first;
+   var fact = (num - 1) * diff;
+   var term = first + fact;
+   return term;
+};
+console.log(findNthTerm(a, b, N));
+*/
---
+++ geomatric nth term/geomatric_pro.js
@@ -0,0 +1,10 @@
+var a = 2, b = 5;
+var N = 7;
+sum = 0;
+value = 2; ratio = 5;
+for(var i=0 ; i<N ; i++){
+    sum = sum + value;
+    value = value * ratio;
+    
+}
+console.log("7th Number of geometric progretion is is" + sum)
